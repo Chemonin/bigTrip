70dd610a7f743ea1c5a55b5b28f3105a385917ac
Chemonin <iachemonin@gmail.com>
2019-09-12 18:20:23 +0300

    save changes

---
+++ src/utils.js
@@ -0,0 +1,37 @@
+export const Position = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+export const formatTime = function (timestamp) {
+  const formatDuration = (time) => ({
+    'D': Math.floor(time / 86400000),
+    'H': time.getUTCHours(),
+    'M': time.getUTCMinutes()
+  });
+  const duration = formatDuration(new Date(timestamp));
+  return `${Object.keys(duration).map((key) => duration[key] > 0 ? `${duration[key]}${key}` : ``).join(` `)}`;
+};
+
+export const render = (parent, child, position) => {
+  switch (position) {
+    case Position.AFTERBEGIN:
+      parent.prepend(child);
+      break;
+    case Position.BEFOREEND:
+      parent.append(child);
+      break;
+  }
+};
+
+export const unrender = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
